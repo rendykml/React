ffe1dca4ce73a629491b130e1c1c41c70a258608
rendykml <rendikamaluddin2018@gmail.com>
2025-07-10 23:22:52 +0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ChatContainer from './components/ChatContainer';
+import HelloWorld from './hello-word/HelloWorld';
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-900">
+    <div className="min-h-screen ">
       <div className="max-w-4xl mx-auto h-screen">
-        <ChatContainer />
+        <HelloWorld />
       </div>
     </div>
   );
