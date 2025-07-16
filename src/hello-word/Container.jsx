55b88426a900317536638e67fb1c8484a9fb107e
rendykml <rendikamaluddin2018@gmail.com>
2025-07-16 23:16:33 +0700

    nambah children

---
+++ src/hello-word/Container.jsx
@@ -0,0 +1,11 @@
+export default function Container({children}) {
+  return (
+    <div>
+      <h1>Rendy Kamaluddin</h1>
+      {children}
+      <footer>
+        <p>2025 @Rendy Kamaluddin</p>
+      </footer>
+    </div>
+  );
+}
