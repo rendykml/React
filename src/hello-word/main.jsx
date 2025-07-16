59c10f9f23e1d0008c7e2e615aa8d80b1bc72e16
rendykml <rendikamaluddin2018@gmail.com>
2025-07-16 23:25:02 +0700

    nested component

--- src/hello-word/main.jsx
+++ src/hello-word/main.jsx
@@ -1,8 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import HelloWorld from "./HelloWorld";
+import Container from "./Container";
 
 createRoot(document.getElementById("root")).render(
 
-    <HelloWorld />
+    <StrictMode>
+        <Container>
+            <HelloWorld/>
+        </Container>
+    </StrictMode>
   
 );
