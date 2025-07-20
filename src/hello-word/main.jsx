bc882c8be90c8eb5b7edad273995385f0ddcefa3
rendykml <rendikamaluddin2018@gmail.com>
2025-07-20 23:00:27 +0700

    push

--- src/hello-word/main.jsx
+++ src/hello-word/main.jsx
@@ -2,12 +2,14 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import HelloWorld from "./HelloWorld";
 import Container from "./Container";
+import Todo from "../todolist/Todo";
 
 createRoot(document.getElementById("root")).render(
 
     <StrictMode>
         <Container>
             <HelloWorld/>
+            <Todo />
         </Container>
     </StrictMode>
   
