0c533e8acf3147fb1d891d2d4b66769a9caccb79
rendykml <rendikamaluddin2018@gmail.com>
2025-07-22 22:41:53 +0700

    push

--- src/table/Table.jsx
+++ src/table/Table.jsx
@@ -1,1 +1,13 @@
-sdf
+import Row from "./Row";
+
+export default function Table({rows}) {
+    return (
+        <table border= "1">
+            <tbody>
+                <Row text="satu"/>
+                <Row text="dua"/>   
+                <Row text="tiga"/>
+            </tbody>
+        </table>
+    );
+}
