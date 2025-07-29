d00addc272b7da2ab1c66bed792ac14304dbb568
rendykml <rendikamaluddin2018@gmail.com>
2025-07-29 23:01:59 +0700

    state

--- src/form/Counter.jsx
+++ src/form/Counter.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export default function Counter() {
   let [counter, setCounter] = useState(0);
@@ -6,12 +6,12 @@ export default function Counter() {
   console.log(`Rendering Counter`);
 
   function handleClick() {
-    setCounter(counter + 1);
+    setCounter(counter + 3);
     console.log(`Counters: ${counter}`);
   }
   return (
     <div>
-      <button onClick={handleClick}>Increment</button>
+      <button onClick={handleClick}>Increment 3 kali</button>
       <h1>Counter: {counter}</h1>
     </div>
   );
