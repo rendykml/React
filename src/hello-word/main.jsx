eba86c113574502dcbe7cd30639e409893f90419
rendykml <rendikamaluddin2018@gmail.com>
2025-07-31 23:47:18 +0700

    push

--- src/hello-word/main.jsx
+++ src/hello-word/main.jsx
@@ -3,13 +3,19 @@ import { StrictMode } from "react";
 import HelloWorld from "./HelloWorld";
 import Container from "./Container";
 import Todo from "../todolist/Todo";
+import Table from "../todolist/Table";
 
 createRoot(document.getElementById("root")).render(
 
     <StrictMode>
         <Container>
-            <HelloWorld/>
-            <Todo />
+            <h1 className="text-3xl font-bold text-center mb-6 text-blue-600">Aplikasi React Saya</h1>
+            <div className="space-y-4">
+                <HelloWorld />
+                <Todo />
+                <Table />
+                {/* ...komponen lain... */}
+            </div>
         </Container>
     </StrictMode>
   
