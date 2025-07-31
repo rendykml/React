3f8fa8ea0b5fe89869447b28b0551e9c159376ac
rendykml <rendikamaluddin2018@gmail.com>
2025-07-31 23:46:14 +0700

    push

--- src/hello-word/Container.jsx
+++ src/hello-word/Container.jsx
@@ -1,13 +1,7 @@
 export default function Container({ children }) {
   return (
-    <div>
-      <h1>Rendy Kamaluddin</h1>
+    <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-lg mt-8">
       {children}
-      <footer>
-        <footer className="text-center text-gray-200 text-xs">
-          <p>© 2023 My First React App</p>
-        </footer>
-      </footer>
     </div>
   );
 }
